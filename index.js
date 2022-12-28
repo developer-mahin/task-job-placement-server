2c8fc9f948c0802cf1967f0b8c863059a548ffc5
developer-mahin <mdmahin1310@gmail.com>
2022-12-28 15:43:42 +0600

    create api for saving user in the database

--- index.js
+++ index.js
@@ -8,6 +8,34 @@ app.use(cors())
 app.use(express.json())
 
 
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.eedstvb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    const usersCollection = client.db("task-job-placement").collection("users")
+
+    try {
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
+run().catch(err => { console.log(err.message) })
+
 app.get("/", async (req, res) => {
     res.send("task server is running")
 })
